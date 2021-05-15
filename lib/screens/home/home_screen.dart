28017be7be7a74086f305863a7d99c00d952865c
lyarevalo <lyarevalo@unal.edu.co>
2021-05-15 17:28:04 -0500

    first screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bikesafe_mobile/widgets/tatiana/default_button.dart';
 import 'package:flutter/material.dart';
 import '../../widgets/tatiana/nav_bar.dart';
 import '../../widgets/tatiana/enums.dart';
@@ -7,7 +8,76 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    body: Center(
+        child: Container(
+        width: MediaQuery.of(context).size.width * 0.7,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            ChoiceChip(
+              avatar: Icon(Icons.local_phone),
+              padding: EdgeInsets.all(10.0),
+              label: Text('Llámame'),
+              selected: true,
+              onSelected: (bool selected) {
+                setState(() {});
+                },
+            ),
+            SizedBox(height: 10),
+            ChoiceChip(
+              avatar: Icon(Icons.remove_red_eye),
+              padding: EdgeInsets.all(10.0),
+              label: Text('Me están siguiendo'),
+              selected: true,
+              onSelected: (bool selected) {
+                setState(() {});
+                },
+            ),
+            SizedBox(height: 10),
+            ChoiceChip(
+              avatar: Icon(Icons.person),
+              padding: EdgeInsets.all(10.0),
+              label: Text('Sujeto extraño'),
+              selected: true,
+              onSelected: (bool selected) {
+                setState(() {});
+                },
+            ),
+            SizedBox(height: 10),
+            ChoiceChip(
+              avatar: Icon(Icons.location_on),
+              padding: EdgeInsets.all(10.0),
+              label: Text('Búscame'),
+              selected: true,
+              onSelected: (bool selected) {
+                setState(() {});
+                },
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                
+                labelText: 'Descripción'
+            )
+            ),
+            SizedBox(height: 10),
+            DefaultButton(
+              text: "Enviar Alerta",
+              press: () {},
+            ),
+            SizedBox(height: 10),
+            
+            ],      
+        ),
+        
+
+      ),
+    ),
       bottomNavigationBar: NavBar(selectedMenu: MenuState.home),
     );
   }
+
+  void setState(Null Function() param0) {}
 }
